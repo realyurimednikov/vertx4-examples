fa9666266e6568e0986fd163fb53f2d38f8b8973
realyurimednikov <mednikov.iurii@gmail.com>
2021-07-29 12:33:06 +0200

    Modified WebClient example

--- src/test/java/me/mednikov/vertx4examples/webclient/WebClientTest.java
+++ src/test/java/me/mednikov/vertx4examples/webclient/WebClientTest.java
@@ -2,17 +2,19 @@ package me.mednikov.vertx4examples.webclient;
 
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
-import io.vertx.core.buffer.Buffer;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.HttpResponse;
 import io.vertx.ext.web.client.WebClient;
-import io.vertx.ext.web.client.predicate.ResponsePredicate;
+import io.vertx.ext.web.codec.BodyCodec;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
 import me.mednikov.vertx4examples.commons.PostModel;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import java.net.URL;
 import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,8 +23,13 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(VertxExtension.class)
 class WebClientTest {
 
-    private static final String BASE_URL = "https://jsonplaceholder.typicode.com/";
+    private static URL BASE_URL;
     private Logger logger = Logger.getLogger("WebClientTest");
+
+    @BeforeAll
+    static void setup() throws Exception{
+        BASE_URL = new URL("https://jsonplaceholder.typicode.com/posts");
+    }
 
     @Test
     void createWebClientTest(Vertx vertx, VertxTestContext context){
@@ -32,16 +39,39 @@ class WebClientTest {
     }
 
     @Test
+    void urlTest(){
+        String result = BASE_URL.toExternalForm();
+        assertEquals("https://jsonplaceholder.typicode.com/posts", result);
+    }
+
+    @Test
     void getRequestTest(Vertx vertx, VertxTestContext context){
         WebClient client = WebClient.create(vertx);
-        String url = BASE_URL.concat("posts");
+        String url = BASE_URL.toExternalForm();
         context.verify(() -> {
-            Future<HttpResponse<Buffer>> result = client.getAbs(url).send();
-            // map body to json
+            Future<HttpResponse<JsonArray>> result = client.getAbs(url).as(BodyCodec.jsonArray()).send();
             result
-                    .flatMap(buffer -> Future.succeededFuture(buffer.bodyAsJsonArray()))
+                    .flatMap(r -> Future.succeededFuture((r.body())))
                     .onSuccess(posts -> {
                         assertEquals(100, posts.size());
+                        context.completeNow();
+                    }).onFailure(context::failNow);
+        });
+    }
+
+    @Test
+    void getWithQueryParamsTest(Vertx vertx, VertxTestContext context) throws Exception{
+        WebClient client = WebClient.create(vertx);
+        String url = new URL("https://jsonplaceholder.typicode.com/comments").toExternalForm();
+        context.verify(() -> {
+            Future<HttpResponse<JsonArray>> result = client.getAbs(url)
+                    .as(BodyCodec.jsonArray())
+                    .addQueryParam("postId", "1")
+                    .send();
+            result
+                    .flatMap(r -> Future.succeededFuture((r.body())))
+                    .onSuccess(posts -> {
+                        assertEquals(5, posts.size());
                         context.completeNow();
                     }).onFailure(context::failNow);
         });
@@ -54,7 +84,7 @@ class WebClientTest {
         payload.put("title", "Nam ullamcorper scelerisque condimentum");
         payload.put("body", "Vivamus accumsan aliquam libero, vitae varius risus dictum sit amet. Quisque vitae maximus orci. Curabitur porta id tellus convallis finibus. Integer semper at augue non bibendum. Quisque eget condimentum mi, quis cursus odio. Mauris mattis mauris vitae mauris eleifend blandit. Ut quis felis ante.");
 
-        String url = BASE_URL.concat("posts");
+        String url = BASE_URL.toExternalForm();
 
         WebClient client = WebClient.create(vertx);
         context.verify(() -> {
@@ -70,7 +100,7 @@ class WebClientTest {
 
     @Test
     void postWithCustomObjectTest(Vertx vertx, VertxTestContext context){
-        String url = BASE_URL.concat("posts");
+        String url = BASE_URL.toExternalForm();
         PostModel post = new PostModel(1, "Donec quis augue semper", "Quisque lacus ligula, lacinia congue blandit ac, varius efficitur ante");
 
         WebClient client = WebClient.create(vertx);
@@ -88,23 +118,10 @@ class WebClientTest {
     @Test
     void responsePredicateTest(Vertx vertx, VertxTestContext context){
         WebClient client = WebClient.create(vertx);
-        String url = BASE_URL.concat("posts/101");
-
-        
-//        context.verify(() -> {
-//            Future<HttpResponse<Buffer>> result = client.getAbs(url).send();
-//            result.onSuccess(response -> {
-//                        logger.info("On success handler is executed");
-//                        context.completeNow();
-//                    }).onFailure(err -> {
-//                        logger.info("On failure handler is executed");
-//                        context.failNow(err);
-//            });
-//        });
+        String url = "https://jsonplaceholder.typicode.com/posts/101";
 
         context.verify(() -> {
-            Future<HttpResponse<Buffer>> result = client.getAbs(url)
-                    .expect(ResponsePredicate.SC_OK)
+            var result = client.getAbs(url)
                     .send();
             result.onSuccess(response -> {
                 logger.info("On success handler is executed");
